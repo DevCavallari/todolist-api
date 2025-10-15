c7af2831d1434f64ccfa03126c770e42ddb6d831
DevCavallari <viniciuscavallari9@gmail.com>
2025-10-14 22:06:23 -0300

    tarefas

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from tarefa import buscar_tarefas
 app = Flask(__name__)
 
 @app.route('/api', methods=['GET'])
@@ -6,7 +7,10 @@ def index():
     return{
         'message': 'Api rodando'
     }
-
+@app.route('/api/tarefas', methods=['GET'])
+def get_tarefas():
+        tarefas = buscar_tarefas()
+        return tarefas
     #Se for o modulo principal roda o projeto em debug
 if __name__ == '__main__':
         app.run(debug=True)
---
+++ tarefa.py
@@ -0,0 +1,18 @@
+from flask import jsonify
+
+def buscar_tarefas():
+        tarefas = [
+        {
+           'id' : 1,
+           'nome' : 'Vamos aumentar o zoom para nao errar a digitacao',
+           'status' : 'Pendente'
+        },
+        {
+            'id' : 2,
+            'nome' : 'Aprender python para programar apis',
+            'status' : 'Pendente'
+        }
+        
+    ]
+        return jsonify(tarefas)
+def buscar_tarefa()
